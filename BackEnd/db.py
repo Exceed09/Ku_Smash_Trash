7e781559b801ebddd91b23ae6280f009ec83696b
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-02-09 19:19:20 +0700

    add mongo db

---
+++ BackEnd/db.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from os import getenv
+
+DATABASE_NAME = "exceed09"
+BIN_STATUS_COLLECTION = "Bin_Status"
+CONTACT_COLLECITON = "Contact"
+MONGO_DB_DOM = "mongo.exceed19.online"
+MONGO_DB_PORT = 8443
+
+load_dotenv(".env")
+username = getenv("name")
+password = getenv("password")
+
+client = MongoClient(f"mongodb://{username}:{password}@{MONGO_DB_DOM}:{MONGO_DB_PORT}/?authMechanism=DEFAULT")
+
+db = client[DATABASE_NAME]
+bin_status = db[BIN_STATUS_COLLECTION]
+contact = db[CONTACT_COLLECITON]
