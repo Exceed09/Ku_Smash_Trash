14cd1c755986a0ac487649f7563f5edbc015ee56
Npwskp <nopawit.sukolpuk@gmail.com>
2023-02-11 03:16:56 +0700

    dont know what to do so I add image

--- Frontend/src/components/Nav.jsx
+++ Frontend/src/components/Nav.jsx
@@ -1,10 +1,13 @@
 import "../styles/Nav.css"
+import { FcFullTrash } from "react-icons/fc"
 
 function Nav() {
   return (
     <div className="navbar">
       <a href="/" className="navbar-a">
-        <h1>KU Smash Trash</h1>
+        <img src="/icon.png" alt="trash" className="trash" width={50}/>
+        <h1 style={{marginLeft:"2rem",marginRight:"2rem"}}>KU Smash Trash</h1>
+        <img src="/icon.png" alt="trash" className="trash" width={50}/>
       </a>
     </div>
   )
