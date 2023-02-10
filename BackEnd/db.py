2e3315ac6612f5c2fed79ee68118ad3c4d5db578
Punn Chunwimaleung <khunpunn2546@gmail.com>
2023-02-10 13:43:08 +0700

    implement add key to line collection

--- BackEnd/db.py
+++ BackEnd/db.py
@@ -5,6 +5,7 @@ from os import getenv
 DATABASE_NAME = "exceed09"
 BIN_STATUS_COLLECTION = "Bin_Status"
 CONTACT_COLLECITON = "Contact"
+LINE_COLLECTION = "Line"
 MONGO_DB_DOM = "mongo.exceed19.online"
 MONGO_DB_PORT = 8443
 
@@ -17,3 +18,4 @@ client = MongoClient(f"mongodb://{username}:{password}@{MONGO_DB_DOM}:{MONGO_DB_
 db = client[DATABASE_NAME]
 bin_status = db[BIN_STATUS_COLLECTION]
 contact = db[CONTACT_COLLECITON]
+line = db[LINE_COLLECTION]
--- BackEnd/routers/line.py
+++ BackEnd/routers/line.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter, HTTPException, Body
 from pydantic import BaseModel
-import db
-import line_notification
+from db import bin_status, contact, line
+from dotenv import load_dotenv
+from cryptography.fernet import Fernet
+import os
+
+load_dotenv(".env")
 
 
 router = APIRouter(
@@ -17,5 +21,9 @@ class Event(BaseModel):
 
 @router.post("/webhook")
 def get_info(event: Event = Body()):
-    print(event)
+    if event.events[0]["type"] == "message" and contact.find_one({"in_charge": event.events[0]["message"]}):
+        fernet = Fernet(os.getenv("fernet_key"))
+        key = fernet.encrypt(event.events[0]["source"]["userID"])
+        line.insert_one({"in_charge": event.events[0]["message"],
+                         "secret_key": key.decode()})
     
