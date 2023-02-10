079ab9265a65d4d683e0471bcd29a43d24ffcaca
Tharunthorn Musik <tharunthornmusik@Tharunthorns-MacBook-Air.local>
2023-02-10 10:22:58 +0700

    Update Line Chatbot Feature

--- BackEnd/line_notification.py
+++ BackEnd/line_notification.py
@@ -2,15 +2,16 @@ from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 from dotenv import load_dotenv
-from os import getenv
+import os
 
 load_dotenv(".env")
-line_API_key = getenv("line_API_key")
-
-line_bot_api = LineBotApi(line_API_key)
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 
 def send_full_status(bin_id: int, zone: str, location: str, status: int):
-    try:
-        line_bot_api.broadcast(TextSendMessage(text=f"Please change garbage bag at Bin {bin_id} in zone {zone} at {location} is {status}% full."))
-    except LineBotApiError as e:
-        raise e
+    if status >= 100:
+        try:
+            line_bot_api.broadcast(TextSendMessage(text=f"Please change garbage bag at Bin {bin_id} in zone {zone} at {location} is {status}% full."))
+        except LineBotApiError as e:
+            raise e
+    else:
+        pass
