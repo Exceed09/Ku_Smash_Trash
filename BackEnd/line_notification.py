b536e751266aa91c11d38d83380dbfad69dd8a27
Tharunthorn Musik <tharunthornmusik@Tharunthorns-MacBook-Air.local>
2023-02-10 11:05:37 +0700

    Update Yellow Status Line Chatbot Feature

--- BackEnd/line_notification.py
+++ BackEnd/line_notification.py
@@ -7,11 +7,34 @@ import os
 load_dotenv(".env")
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 
-def send_full_status(bin_id: int, zone: str, location: str, status: int):
+def send_full_alert(bin_id: int, zone: str, location: str, status: int):
     if status >= 100:
         try:
-            line_bot_api.broadcast(TextSendMessage(text=f"Please change garbage bag at Bin {bin_id} in zone {zone} at {location} is {status}% full."))
+            line_bot_api.broadcast(TextSendMessage(text=f"Please change garbage bag at Bin {bin_id} in zone {zone} at {location} is {status}% Full."))
         except LineBotApiError as e:
             raise e
     else:
         pass
+
+def sent_red_alert(bin_id: int, zone: str, location: str, status: int):
+    if status >= 80 and status < 100:
+        try:
+            line_bot_api.broadcast(TextSendMessage(text=f"Please Check Bin {bin_id} in zone {zone} at {location} is {status}% Almostfull."))
+        except LineBotApiError as e:
+            raise e
+    else:
+        pass
+
+def sent_yellow_alert(bin_id: int, zone: str, location: str, status: int):
+    if status >= 50 and status < 80:
+        try:
+            line_bot_api.broadcast(TextSendMessage(text=f"Please Standby Bin {bin_id} in zone {zone} at {location} is {status}% Half full."))
+        except LineBotApiError as e:
+            raise e
+    else:
+        pass
+
+def sent_message(bin_id: int, zone: str, location: str, status: int):
+    send_full_alert(bin_id, zone, location, status)
+    sent_red_alert(bin_id, zone, location, status)
+    sent_yellow_alert(bin_id, zone, location, status)
