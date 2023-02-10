412183cb18d6717d9495b8b915f7e4a3cb93b478
Punn Chunwimaleung <khunpunn2546@gmail.com>
2023-02-10 13:51:51 +0700

    fix wrong key name and missing encode

--- BackEnd/routers/line.py
+++ BackEnd/routers/line.py
@@ -23,7 +23,7 @@ class Event(BaseModel):
 def get_info(event: Event = Body()):
     if event.events[0]["type"] == "message" and contact.find_one({"in_charge": event.events[0]["message"]["text"]}):
         fernet = Fernet(os.getenv("fernet_key").encode())
-        key = fernet.encrypt(event.events[0]["source"]["userID"])
+        key = fernet.encrypt(event.events[0]["source"]["userId"].encode())
         line.insert_one({"in_charge": event.events[0]["message"],
                          "secret_key": key.decode()})
     
